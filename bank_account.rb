300f63aec227abf78421df9f429045b1fb14839f
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 11:22:45 -0700

    add credit method

--- bank_account.rb
+++ bank_account.rb
@@ -1,9 +1,26 @@
 class BankAccount 
+ attr_reader :name    #read bank accout name 
  def intiialize(name) #set argument 
   @name = name        #instantiate instance of bank account with name 
   @transactions = []  #empty array of transactions 
+  add_transaction("Beginning Balance", 0) 
  end 
+
+ def credit(description, amount) #set arguments 
+  add_transaction(description,amount) #call add_transaction method 
+ end 
+
+ def add_transaction(description, amount) 
+ #append to list of transactions 
+  @transactions.push(description: description, amount: amount) #push an array of description and amount keys 
+ end 
+
 end 
+
+#########################################################################
+bank_account = BankAccount.new("Ophelia")
+puts bank_account.inspect 
+bank_account.add_transaction("Groceries", 20) 
 
 
 
