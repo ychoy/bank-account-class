903826d0a41899a3336cf12c1f77349904da0e32
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 11:16:32 -0700

    define the classs and internal variable

---
+++ bank_account.rb
@@ -0,0 +1,9 @@
+class BankAccount 
+ def intiialize(name) #set argument 
+  @name = name        #instantiate instance of bank account with name 
+  @transactions = []  #empty array of transactions 
+ end 
+end 
+
+
+
