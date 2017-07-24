0a8c02b8244c095194bfc0c2706ee3bd8a7b5e7e
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 11:28:33 -0700

    fix spelling error and call credit and debit method

--- bank_account.rb
+++ bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount 
  attr_reader :name    #read bank accout name 
- def intitialize(name) #set argument 
+ def initialize(name) #set argument 
   @name = name        #instantiate instance of bank account with name 
   @transactions = []  #empty array of transactions 
   add_transaction("Beginning Balance", 0) 
@@ -25,8 +25,9 @@ end
 #########################################################################
 bank_account = BankAccount.new("Ophelia")
 puts bank_account.inspect 
-bank_account.add_transaction("Groceries", -20)
+bank_account.credit("Painting Sale", 500)
+bank_account.debit("Groceries", -20)
+puts "########################################"
 puts bank_account.inspect  
-
 
 
