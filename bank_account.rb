115c1db68ed8ddc467c427e84929f1ab2abb55ff
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-07-24 11:37:26 -0700

    create balance method that calculates balance and prints with special format

--- bank_account.rb
+++ bank_account.rb
@@ -14,20 +14,32 @@ class BankAccount
   add_transaction(description, -amount) #add negative amount to indicate purchase 
  end 
  
-
  def add_transaction(description, amount) 
  #append to list of transactions 
   @transactions.push(description: description, amount: amount) #push an array of description and amount keys 
+ end 
+
+ def balance
+ #loop through transactions to check balance 
+
+  balance = 0.0 
+  @transactions.each do |transaction| 
+   balance += transaction[:amount] #increment local variable balance with each transaction amount 
+  end 
+
+  return balance 
+ end 
+
+ def to_s
+  "Name: #{name}, Balance: #{sprintf("%0.2f", balance)}" 
+  #use sprintf to format with two decimals 
+  #use string interpolation 
  end 
 
 end 
 
 #########################################################################
 bank_account = BankAccount.new("Ophelia")
-puts bank_account.inspect 
 bank_account.credit("Painting Sale", 500)
 bank_account.debit("Groceries", -20)
-puts "########################################"
-puts bank_account.inspect  
-
-
+puts bank_account
